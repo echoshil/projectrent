bd21bb05727ef7fe1c14399edb434739d7271281
ikiw <163365900+echoshil@users.noreply.github.com>
2025-11-18 22:11:59 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,6 @@
-import serverless from "serverless-http";
 import { createServer } from "../server/index.ts";
 
-let handler;
-
-async function getHandler() {
-  if (!handler) {
-    const app = await createServer();
-    handler = serverless(app);
-  }
-  return handler;
+export default async function handler(req, res) {
+  const app = await createServer();
+  app(req, res);
 }
-
-export default async function (req, res) {
-  const handle = await getHandler();
-  return handle(req, res);
-}
-
